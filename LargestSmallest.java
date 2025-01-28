97e10f1733177635fed9fc5c743b86f10c68adb3
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 21:58:22 +0530

    Array largest and smallest element

---
+++ LargestSmallest.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class LargestSmallest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int largest = findLargest(array);
+        int smallest = findSmallest(array);
+
+        System.out.println("Largest element: " + largest);
+        System.out.println("Smallest element: " + smallest);
+
+        scanner.close();
+    }
+
+    public static int findLargest(int[] array) {
+        int largest = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int findSmallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+}
+
+
+
